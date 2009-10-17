8980ecd37bc6216a560e363fc2f993d872a0f9b7
Michael Koby <michael.koby@gmail.com>
2009-10-16 19:47:11 -0500

    Added a file to seperate out the Saved Search Methods.

---
+++ src/TwitterNET/REST/SavedSearchMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace TwitterNET
+{
+    public partial class Twitter
+    {
+        public SavedSearch CreateSavedSearch(string searchQuery)
+        {
+            string apiURL = "http://twitter.com/saved_searches/create.xml";
+            string responseText = _requestHandler.MakeAPIRequest(_requestHandler,
+                        String.Format("{0}?query={1}", apiURL, HttpUtility.UrlEncode(searchQuery)));
+
+            return ResponseParser.ReturnSingleSavedSearch(responseText);
+        }
+
+        public SavedSearch DeleteSavedSearch(long id)
+        {
+            string apiURL = "http://twitter.com/saved_searches/destroy/";
+            string responseText = _requestHandler.MakeAPIRequest(_requestHandler, String.Format("{0}{1}.xml", apiURL, id));
+
+            return ResponseParser.ReturnSingleSavedSearch(responseText);
+        }
+
+        public IList<SavedSearch> GetSavedSearches()
+        {
+            string apiURL = "http://twitter.com/saved_searches.xml";
+            string responseText = _requestHandler.MakeAPIRequest(_requestHandler, apiURL);
+
+            return ResponseParser.ReturnListOfSavedSearches(responseText);
+        }
+
+        public SavedSearch ShowSavedSearch(long id)
+        {
+            string apiURL = "http://twitter.com/saved_searches/show/";
+            string responseText = _requestHandler.MakeAPIRequest(_requestHandler, String.Format("{0}{1}.xml", apiURL, id));
+
+            return ResponseParser.ReturnSingleSavedSearch(responseText);
+        }
+    }
+
+}
--- src/TwitterNET/Twitter.cs
+++ src/TwitterNET/Twitter.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Web;
-using System.Xml.Linq;
 
 namespace TwitterNET
 {
@@ -74,40 +69,6 @@ namespace TwitterNET
         public IUser GetSingleUser(long userID)
         {
             throw new NotImplementedException();
-        }
-
-        public SavedSearch CreateSavedSearch(string searchQuery)
-        {
-            string apiURL = "http://twitter.com/saved_searches/create.xml";
-            string responseText = _requestHandler.MakeAPIRequest(_requestHandler, 
-                        String.Format("{0}?query={1}", apiURL, HttpUtility.UrlEncode(searchQuery)));
-
-            return ResponseParser.ReturnSingleSavedSearch(responseText);
-        }
-
-        public SavedSearch DeleteSavedSearch(long id)
-        {
-            string apiURL = "http://twitter.com/saved_searches/destroy/";
-            string responseText = _requestHandler.MakeAPIRequest(_requestHandler, String.Format("{0}{1}.xml", apiURL, id));
-
-            return ResponseParser.ReturnSingleSavedSearch(responseText);
-        }
-
-        public IList<SavedSearch> GetSavedSearches()
-        {
-            string apiURL = "http://twitter.com/saved_searches.xml";
-            string responseText = _requestHandler.MakeAPIRequest(_requestHandler, apiURL);
-
-            return ResponseParser.ReturnListOfSavedSearches(responseText);
-        }
-
-        public SavedSearch ShowSavedSearch(long id)
-        {
-            string apiURL = "http://twitter.com/saved_searches/show/";
-            string responseText = _requestHandler.MakeAPIRequest(_requestHandler, String.Format("{0}{1}.xml", apiURL, id));
-
-            return ResponseParser.ReturnSingleSavedSearch(responseText);
-
         }
     }
 }
